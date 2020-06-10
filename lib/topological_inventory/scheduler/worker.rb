24a826e1a9fe3b28a665f22d8bfd6d2181678060
Martin Slemr <mslemr@redhat.com>
2020-06-10 18:28:21 +0200

    Invoke task refreshes grouped by source

--- lib/topological_inventory/scheduler/worker.rb
+++ lib/topological_inventory/scheduler/worker.rb
@@ -6,7 +6,7 @@ module TopologicalInventory
     class Worker
       include Logging
 
-      JOB_REFRESH_QUEUE_NAME = "platform.topological-inventory.collector-ansible-tower".freeze
+      REFRESH_QUEUE_NAME = "platform.topological-inventory.collector-ansible-tower".freeze
 
       def initialize(opts = {})
         messaging_client_opts = opts.select { |k, _| %i[host port].include?(k) }
@@ -15,45 +15,61 @@ module TopologicalInventory
 
       # TODO: schedule also full refreshes
       def run
-        loop do
+        # loop do
           tasks = load_running_tasks
 
           # TODO: refresh only for available sources
           invoke_targeted_refresh(tasks)
 
-          sleep(10)
-        end
+          # sleep(10)
+        # end
       end
 
       private
 
+      attr_accessor :messaging_client_opts
+
       def invoke_targeted_refresh(tasks)
-        payload = []
+        payload = {}
 
         tasks.each do |task|
+          # grouping requests by Source
+          # - tasks are ordered by source_id
+          if payload[:source_id].present? && payload[:source_id] != task.source_id
+            send_payload(payload)
+            payload = {}
+          end
+
+          payload[:source_id] = task.source_id.to_s
+          payload[:source_uid] = task.source_uid.to_s
+
           logger.info("Publishing ServiceInstance:SourceRef: #{task.target_source_ref}, Task: #{task.id}")
-          payload <<
-            {
-              :request_context => task.forwardable_headers,
-              :source_id       => task.source_id.to_s,
-              :source_uid      => task.source_uid.to_s,
-              :source_ref      => task.target_source_ref
-            }
+          payload[:params] ||= []
+          payload[:params] << {
+            :request_context => task.forwardable_headers,
+            :source_ref      => task.target_source_ref,
+            :task_id         => task.id
+          }
         end
 
-        messaging_client.publish_topic(
-          :service => JOB_REFRESH_QUEUE_NAME,
-          :event   => "ServiceInstance.refresh",
-          :payload => payload.to_json
-        )
+        # sending remaining data
+        send_payload(payload) if payload[:params].present?
       end
 
       def load_running_tasks
         Task.where(:state => 'running', :target_type => 'ServiceInstance')
-          .joins(:source).select('tasks.*, sources.uid as source_uid')
+          .joins(:source)
+          .select('tasks.id, tasks.source_id, tasks.target_source_ref, tasks.forwardable_headers, sources.uid as source_uid')
+          .order('source_id')
       end
 
-      attr_accessor :messaging_client_opts
+      def send_payload(payload)
+        messaging_client.publish_topic(
+          :service => REFRESH_QUEUE_NAME,
+          :event   => "ServiceInstance.refresh",
+          :payload => payload.to_json
+        )
+      end
 
       def messaging_client
         @messaging_client ||= ManageIQ::Messaging::Client.open(messaging_client_opts)
